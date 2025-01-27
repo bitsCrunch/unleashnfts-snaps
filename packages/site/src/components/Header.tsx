04dfca360bff0500630523c2e490c930e0fff150
pranesh kamthe <pranesh@bitscrunch.com>
2025-01-27 13:58:33 +0530

    logo updates

--- packages/site/src/components/Header.tsx
+++ packages/site/src/components/Header.tsx
@@ -47,7 +47,6 @@ export const Header = ({
     <HeaderWrapper>
       <LogoWrapper>
         <SnapLogo color={theme.colors.icon?.default} size={36} />
-        <Title>unleashNFTs Snap</Title>
       </LogoWrapper>
       <RightContainer>
         <Toggle
